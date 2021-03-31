1267f7e2977998d950d79901e5593907007235ed
pacmanzou <pacmanzou@qq.com>
2021-03-31 21:15:01 +0800

    del sh for browser

--- pacmanzou/Scripts/misc/multiple_task.sh
+++ pacmanzou/Scripts/misc/multiple_task.sh
@@ -3,7 +3,7 @@
 # Description: Record what you're going to do
 # Time: 22:32:35 2020-11-06
 
-file="$HOME/.todo"
+file="$HOME/.task"
 
 if [[ ! -f "${file}" ]]; then
     touch "$file"
@@ -13,18 +13,18 @@ _dmenu() {
     dmenu -i -b -l 14 -nb '#000000' -nf '#ebdbb2' -sf '#ebdbb2' -sb '#353535' -fn '' "$@"
 }
 
-add_todo() {
+add_task() {
     echo -e "$(date +"%m-%d")   " "$cmd" >>"$file"
 }
 
-cmd=$(_dmenu -p "add" <"$file")
+cmd=$(_dmenu -p "task" <"$file")
 
 while [ -n "$cmd" ]; do
     if grep -q "^$cmd\$" "$file"; then
         grep -v "^$cmd\$" "$file" >"$file.$$"
         mv "$file.$$" "$file"
     else
-        add_todo
+        add_task
     fi
-    cmd=$(_dmenu -p "add" "$@" <"$file")
+    cmd=$(_dmenu -p "task" "$@" <"$file")
 done
