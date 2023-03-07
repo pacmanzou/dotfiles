e6bbce65fdc92006668dbe0d1b135b01b0e37738
pacmanzou <pacmanzou@qq.com>
2023-03-07 14:41:40 +0800

    del monitorbattery.sh

--- link/home/.i3/background.sh
+++ link/home/.i3/background.sh
@@ -4,10 +4,7 @@
 
 while :; do
 	sleep 1h
+
 	wget -O "$HOME/.i3/background.png" https://source.unsplash.com/1920x1080
-	killall conky
-	sleep 1
 	nitrogen --set-zoom-fill "$HOME/.i3/background.png"
-	sleep 1
-	conky -c "$HOME/.i3/conky_i3wm"
 done
