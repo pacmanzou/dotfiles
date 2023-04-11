a62c2749605d7ae67d80de6b8d27a74712891647
pacmanzou <pacmanzou@qq.com>
2023-04-12 00:37:46 +0800

    rename background -> wallpaper

---
+++ link/home/.i3/wallpaper.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Copyright (C) 2023 pacmanzou <pacmanzou@qq.com>
+# Description: Automatically change wallpaper
+
+url="https://source.unsplash.com/1920x1080/?wallpaper"
+wallpaper="$HOME/.i3/wallpaper.png"
+
+while :; do
+	sleep 1h
+
+	curl -L -s -o "$wallpaper" "$url"
+	nitrogen --set-zoom-fill "$wallpaper"
+done
