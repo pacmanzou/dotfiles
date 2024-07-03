9ee4792469de5bd88f3b124bb037d8c35875b6ba
pacmanzou <pacmanzou@qq.com>
2024-07-03 15:16:20 +0800

    update nvim plugins

--- link/home/.i3/wallpaper.sh
+++ link/home/.i3/wallpaper.sh
@@ -2,11 +2,11 @@
 # Copyright (C) 2023 pacmanzou <pacmanzou@qq.com>
 # Description: Automatically change wallpaper
 
-url="https://source.unsplash.com/1920x1080/?random"
+url="https://api.nguaduot.cn/wallhaven/random"
 wallpaper="$HOME/.i3/wallpaper.png"
 
 while :; do
 	sleep 1h
-	curl -L -s -o "$wallpaper" "$url"
+	curl -L --fail -s -o "$wallpaper" "$url"
 	nitrogen --set-zoom-fill "$wallpaper"
 done
